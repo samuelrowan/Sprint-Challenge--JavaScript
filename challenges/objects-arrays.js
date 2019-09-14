9d19f9767fee81857d755831547ee797bf023aa0
Samuel Rowan <christbro@gmail.com>
2019-09-13 20:16:08 -0500

    Possible solution done for unisWithUni

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -89,6 +89,7 @@ console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+  unisWithUni.push(raduates.filter(university => university.includes('uni')));
 console.log(unisWithUni);
 
 
