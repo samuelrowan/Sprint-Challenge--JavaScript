ab05a0ab36b54814918dff9c30a5300111df3e35
Samuel Rowan <christbro@gmail.com>
2019-09-15 18:17:20 -0500

    fixed error on line 151

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -147,8 +147,8 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
-populationTotal.push(zooAnimals.reduce((total, animal) => total + animal.population, 0))
+const populationTotal = zooAnimals.reduce((total, animal) => total + animal.population, 0);
+
 console.log(populationTotal);
 
 
